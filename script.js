d2baba161c164d13c7616a64c67363a4500c343e
Christian Koziel <christiankoziel@hotmail.com>
2024-10-22 22:55:22 -0300

    Aula Formulário

--- script.js
+++ script.js
@@ -1,36 +1,17 @@
-// Seletor de elemento por id
-function puxarElementoPorId() {
-    const titulo = document.getElementById('meuTitulo');
-    titulo.textContent = "Titulo Alterado";
-    //console.log(titulo);
-}
+document.getElementById('emailForm').addEventListener("submit",
+    function(event) {
+        event.preventDefault();
 
-// Seletor de elemento por Classe
-function puxarElementoPorClasse() {
-    const subtitulo = document.getElementsByClassName('subtitulo');
-    console.log(subtitulo);
-}
+        const to = document.getElementById("to").value;
+        const subject = document.getElementById("subject").value;
+        const body = document.getElementById("body").value;
 
-// Seletor puxar elemento por tag
-function puxarElementoPorTag() {
-    const paragrafos = document.getElementsByTagName('p');
-    console.log(paragrafos);
-}
+        const mailtolink = `mailto:${to}?subject=
+        ${encodeURIComponent(subject)}
+        &body=${encodeURIComponent(body)}
+        `
 
-// Função para criar a lista
-function criarLista() {
-    const container = document.getElementById('lista-container');
-    container.innerHTML =
-    `
-    <ul>
-        <li>Item 1</li>
-        <li>Item 2</li>
-        <li>Item 3</li>
-    </ul>
-    `;
-}
+        window.location.href = mailtolink;
 
-function removerLista() {
-    const container = document.getElementById('lista-container');
-    container.innerHTML = ``;
-}
+    }
+)
